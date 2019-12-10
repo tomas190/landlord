5b8f0dd1fb94fbfe78c44670df53ec47e66beb11
fido <fido@gamstudio.net>
2019-12-10 14:22:11 +0800

    机器人掉线率调整到0.001

--- game/z_robot_step2_playing.go
+++ game/z_robot_step2_playing.go
@@ -11,12 +11,12 @@ func RobotPlayAction(room *Room, robot, nextPlayer, lastPlayer *Player) {
 	isFakerDisconnection := delayDestiny()
 	logger.Debug("机器人打牌阶段...")
 	if isFakerDisconnection { // 如果概率出现了 假装掉线 则配合不出操作 并且机器人以后走托管流程
-		logger.Debug("机器人打牌阶段 中1%概率掉线托管...")
+		logger.Debug("机器人打牌阶段 中0.001%概率掉线托管...")
 		robot.IsGameHosting = true
 		RespGameHosting(room, playerStatus.GameHosting, robot.PlayerPosition, robot.PlayerInfo.PlayerId)
 		if robot.IsMustDo {
 			DoGameHosting(room, robot, nextPlayer, lastPlayer)
-		}else {
+		} else {
 			NotOutCardsAction(room, robot, lastPlayer, nextPlayer)
 		}
 		return
@@ -26,30 +26,31 @@ func RobotPlayAction(room *Room, robot, nextPlayer, lastPlayer *Player) {
 
 // 机器人出牌决策等待时间
 func delayDestiny() bool {
-	destiny := RandNum(1, 100)
-	if destiny <= 70 {
+	destiny := RandNum(1, 10000)
+	if destiny <= 7000 {
 		logger.Debug("机器人打牌阶段决策时间:快速 1-2s")
 		DelaySomeTime(getWaitTimeOutCardFast())
 		return false
 	}
 
-	if destiny <= 85 {
+	if destiny <= 8500 {
 		logger.Debug("机器人打牌阶段决策时间:中速 3-5s")
 		DelaySomeTime(getWaitTimeOutCardMedium())
 		return false
 	}
 
-	if destiny <= 92 {
+	if destiny <= 9200 {
 		logger.Debug("机器人打牌阶段决策时间:慢速 6-15s")
 		DelaySomeTime(getWaitTimeOutCardSlowly())
 		return false
 	}
 
-	if destiny <= 99 {
+	if destiny <= 9999 {
 		logger.Debug("机器人打牌阶段决策时间:超慢 15-29s")
 		DelaySomeTime(getWaitTimeOutCardSoSlowly())
 		return false
 	}
+	// todo 建议概率一万分之1
 	logger.Debug("机器人打牌阶段决策时间:假装断线 30s")
 	DelaySomeTime(getWaitTimeOutCardFakerToDisconnection())
 	return true
