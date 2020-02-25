980c71bb32fa1d0a929076a77ce4698d0987a946
fido <fido@gamstudio.net>
2020-02-25 14:52:44 +0700

    先注释取消服务退出信号量监测

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/wonderivan/logger"
 	"landlord/app"
-	"landlord/exitdo"
-	"landlord/game"
-	"os"
 )
 
 func main() {
-	go app.StartServer(gin.DebugMode, "/ws")
-	logger.Debug("程序启动 监听信号量...")
-	exitdo.Signal.ListenKill().Done(func(sig os.Signal) {
-		logger.Debug("程序关闭:",sig)
-		game.BackUserToHall()
-	})
+	app.StartServer(gin.DebugMode, "/ws")
+	//go app.StartServer(gin.DebugMode, "/ws")
+	//logger.Debug("程序启动 监听信号量...")
+	//exitdo.Signal.ListenKill().Done(func(sig os.Signal) {
+	//	logger.Debug("程序关闭:",sig)
+	//	game.BackUserToHall()
+	//})
 }
